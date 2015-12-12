0112f1d6cf97949d441ba02ada4fb8c59216b54b
smartinov <stefan.martinov@gmail.com>
2015-12-12 18:40:40 +0100

    Add diagram and table model

--- sqlviewer/viewer/models.py
+++ sqlviewer/viewer/models.py
@@ -1,3 +1,16 @@
-from django.db import models
-
 # Create your models here.
+class Diagram:
+    def __init__(self, identifier, name, tables):
+        self.id = identifier
+        self.name = name
+        self.tables = tables
+
+
+class Table:
+    def __init__(self, identifier, name, pos_x, pos_y, width, height):
+        self.id = identifier
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.width = width
+        self.height = height
