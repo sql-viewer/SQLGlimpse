518dbb7e9080bb8ea28531b314971af9376db6a4
Stefan Martinov <stefan.martinov@gmail.com>
2015-12-13 15:19:21 +0100

    Add test

--- sqlviewer/viewer/tests.py
+++ sqlviewer/viewer/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
-
 # Create your tests here.
+import unittest
+from django.test import TestCase, Client
+
+
+class RestAPITest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/api/v1/models/1/diagrams')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
