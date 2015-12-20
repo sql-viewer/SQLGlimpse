2e3c0e78a00cad05f38aaebd69a29a36ae693980
Stefan Martinov <stefan.martinov@gmail.com>
2015-12-20 19:53:28 +0100

    Minor, committing work

--- tests/test_import.py
+++ tests/test_import.py
@@ -11,9 +11,9 @@ class TestMySQLWbImport(TestCase):
         self.maxDiff = None
 
     def test_import_model(self):
-        model_path = get_resource_path('model/mysqlwb.mwb')
+        model_path = get_resource_path('model/mysqlwb-small.mwb')
         data = mysqlwb.import_model(model_path, 'name', 'version')
-        with open(get_resource_path('model/mysqlwb.json')) as fin:
+        with open(get_resource_path('model/mysqlwb-small.json')) as fin:
             expected_data = json.load(fin)
 
         self.assertDictEqual(expected_data, data)
