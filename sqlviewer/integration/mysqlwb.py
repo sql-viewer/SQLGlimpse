7871678ce73a00c1d7f537fe9b69243844ed5264
Stefan Martinov <stefan.martinov@gmail.com>
2015-12-20 19:43:42 +0100

    Add Data-Column Parsing

--- sqlviewer/integration/mysqlwb.py
+++ sqlviewer/integration/mysqlwb.py
@@ -10,8 +10,15 @@ def import_model(model_path, name, version):
 
     model_element = root.find('.//value[@struct-name="workbench.logical.Model"]')
     model = parse_model_information(model_element, name, version)
+
     for table_element in root.findall('.//value[@type="object"][@struct-name="db.mysql.Table"]'):
-        model['data']['tables'].append(parse_table_information(table_element))
+        table = parse_table_information(table_element)
+        model['data']['tables'].append(table)
+
+        query = '.value[@key="columns"]/value[@struct-name="db.mysql.Column"]'
+        for idx, column_element in enumerate(table_element.findall(query)):
+            column = parse_column_information(column_element, table['id'], idx)
+            table['columns'].append(column)
 
     return {"model": model}
 
@@ -37,20 +44,24 @@ def parse_table_information(table_element):
     }
 
 
-def parse_column_information(column_element, table_id):
-    return {
+def parse_column_information(column_element, table_id, ordinal):
+    column = {
         "id": column_element.get('id').strip('{}'),
         "tableId": table_id,
         "name": column_element.find('.value[@key="name"]').text,
         "comment": column_element.find('.value[@key="comment"]').text,
-        "flags": {
-            "key": False,
-            "nullable": int(column_element.find('.value[@key="isNotNull"]').text) is 0,
-            "autoIncrement": False,
-            "hidden": False,
-            "reference": False
-        }
     }
+    flags = {
+        "key": False if ordinal else True,
+        "nullable": int(column_element.find('.value[@key="isNotNull"]').text) is 0,
+        "autoIncrement": False,
+        "hidden": False,
+        "reference": column['name'].endswith('_RefID')
+    }
+
+    column['flags'] = flags
+
+    return column
 
 
 def read_model_from_zip(path):
