78555ad87314c4296b830503631380b80ef9e1e8
lazicnemanja <nemanja.lazic87@gmail.com>
2016-01-04 21:22:22 +0100

    added titles on hover

--- static/js/sql-viewer/table.js
+++ static/js/sql-viewer/table.js
@@ -12,27 +12,40 @@ SqlViewer.Table = function (x, y, data) {
 };
 
 SqlViewer.Table.prototype.draw = function() {
-    SqlViewer.draw.svg(this.createTable(this.data));
+    SqlViewer.draw.svg(this.createTable());
 }
 
-SqlViewer.Table.prototype.createTable = function(data) {
+SqlViewer.Table.prototype.createTable = function() {
 
     table = SqlViewer.stringFormat('<foreignobject x="{0}" y="{1}">', this.x, this.y);
 
-    table += SqlViewer.stringFormat("<table class='sqlv-table' data-toggle='tooltip' title='{1}'><tr><th>{0}</th></tr>", 
-        data.name, 
-        data.comment);
+    table += SqlViewer.stringFormat("<table class='sqlv-table' data-toggle='tooltip' title='{1}' id='{2}'>"
+        + "<tr><th>{0}</th></tr>", 
+        this.data.name, 
+        this.data.comment,
+        this.data.id
+    );
 
     for (var i = 0; i < this.data.columns.length; i++) {
 
         var column = this.data.columns[i];
-
         table += SqlViewer.stringFormat("<tr data-toggle='tooltip' title='{1}' ><td>{0}</td></tr>", 
             column.name, 
-            column.comment);
+            this.createRowTitle(column));
     };
     table += "</table></foreignobject>";
 
     return table;
 }
 
+SqlViewer.Table.prototype.createRowTitle = function(column) {
+
+    var type = column.customType || column.dataType;
+
+    separator = "";
+    for (var i = type.length+5; i >= 0; i--) {
+        separator += "- ";
+    };
+
+    return SqlViewer.stringFormat("{0}&#xA;{1}&#xA;{2}", type, separator, column.comment.replace("\n", "&#xA;"));
+}
