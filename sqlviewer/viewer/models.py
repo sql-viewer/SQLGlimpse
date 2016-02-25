97d43f661d2faa3c30848481ca0d290016baeaa5
Stefan Martinov <stefan.martinov@gmail.com>
2016-02-26 00:14:12 +0100

    Add all classes to models with abstractions with the initial migration script

--- sqlviewer/viewer/models.py
+++ sqlviewer/viewer/models.py
@@ -2,21 +2,25 @@ import uuid
 from django.db import models
 
 
-class Model(models.Model):
+class UUIDModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Model(UUIDModel):
     name = models.CharField(max_length=128, blank=False, null=False)
     version = models.CharField(max_length=128, blank=False, null=False)
 
 
-class Diagram(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Diagram(UUIDModel):
     name = models.CharField(max_length=128, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
     model = models.ForeignKey(Model)
 
 
-class Table(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Table(UUIDModel):
     name = models.CharField(max_length=128, blank=False, null=False)
     comment = models.TextField(blank=True, null=True)
     model = models.ForeignKey(Model)
@@ -25,8 +29,7 @@ class Table(models.Model):
         return Column.objects.filter(table=self)
 
 
-class Column(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Column(UUIDModel):
     name = models.CharField(max_length=128, blank=False, null=False)
     comment = models.TextField(blank=True, null=True)
     table = models.ForeignKey(Table)
@@ -37,13 +40,51 @@ class Column(models.Model):
     is_hidden = models.BooleanField(default=False)
 
 
-class ForeignKey(models.Model):
+class ForeignKey(UUIDModel):
     TYPE_COLUMN = 'column'
     TYPE_TABLE = 'table'
+    TYPE_CHOICES = (
+        (TYPE_COLUMN, 'column'),
+        (TYPE_TABLE, 'table')
+    )
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    type = models.CharField(max_length=1, choices=(TYPE_COLUMN, TYPE_TABLE), default=TYPE_COLUMN)
-    target_table = models.ForeignKey(Table)
-    target_column = models.ForeignKey(Column, null=True)
-    source_table = models.ForeignKey(Table)
-    source_column = models.ForeignKey(Column, null=True)
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default=TYPE_COLUMN)
+    target_table = models.ForeignKey(Table, related_name='target_table')
+    target_column = models.ForeignKey(Column, null=True, related_name='target_column')
+    source_table = models.ForeignKey(Table, related_name='source_table')
+    source_column = models.ForeignKey(Column, null=True, related_name='source_column')
+
+
+class AbstractElement(UUIDModel):
+    pos_x = models.IntegerField()
+    pos_y = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    color = models.CharField(max_length=7)
+
+    class Meta:
+        abstract = True
+
+
+class LayerElement(AbstractElement):
+    name = models.CharField(max_length=128, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    diagram = models.ForeignKey(Diagram)
+
+
+class TableElement(AbstractElement):
+    table = models.ForeignKey(Table)
+    collapsed = models.BooleanField(default=False)
+
+
+class ConnectionElement(UUIDModel):
+    DRAW_SPLIT = "split"
+    DRAW_FULL = "full"
+    DRAW_HIDDEN = "hidden"
+    DRAW_CHOICES = (
+        (DRAW_SPLIT, "split"),
+        (DRAW_FULL, "full"),
+        (DRAW_HIDDEN, "hidden")
+    )
+    foreignKey = models.ForeignKey(ForeignKey)
+    draw = models.CharField(max_length=10, choices=DRAW_CHOICES, default=DRAW_FULL)
