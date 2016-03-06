21e9c4976e6fb5fdd41ea7e7818c8bcc90bb3287
Nemanja Lazic <nemanja.lazic87@gmail.com>
2016-03-06 21:48:04 +0100

    added footer

--- static/js/sql-viewer/events.js
+++ static/js/sql-viewer/events.js
@@ -7,3 +7,14 @@ $( document ).click(function(e) {
 		$(e.target).addClass("sqlv-table-selected");
 	}
 });
+
+$("#footer-header").click(function() {
+    if ( $("#footer").height() != 30) {
+    	$( this ).text('see more');
+        $( "#footer" ).animate({ height: 30 }, 600 );
+    }
+    else {
+    	$( this ).text('see less');
+    	$( "#footer" ).animate({ height: 350 }, 600 );
+    }
+});
