70d09281e9f6da37db064531ae2515bd8171cfd5
Nemanja Lazic <nemanja.lazic87@gmail.com>
2016-03-09 21:40:31 +0100

    Connection X coordinate fix

--- static/js/sql-viewer/events.js
+++ static/js/sql-viewer/events.js
@@ -21,6 +21,13 @@ $("#footer-header").click(function() {
 
 function setEvents() {
     
+    setEventsForFullConnections();
+
+    setEventsForSplitConnections();
+
+}
+
+function setEventsForFullConnections() {
     var hoverClass = "sqlv-selected-row-hover";
     var selectedClass = "sqlv-selected-row";
 
@@ -49,6 +56,45 @@ function setEvents() {
     });
 }
 
+function setEventsForSplitConnections() {
+    var hoverClass = "sqlv-selected-row-hover";
+    var selectedClass = "sqlv-selected-row";
+
+    $(".svg-link-split").mouseover(function() {
+        var data = $(this).data();
+        selectLinkSplit(data,hoverClass);
+    }); 
+
+    $(".svg-link-split").mouseout(function() {
+        var data = $(this).data();
+        deselectLinkSplit(data, hoverClass);
+    });
+
+    $(".svg-link-split").click(function() {
+        var data = $(this).data();
+        var hasSelectedClass = sessionStorage.getItem($(this).attr('id'));
+
+        if (hasSelectedClass == null || hasSelectedClass == "false") {
+            selectLinkSplit(data, selectedClass); 
+            sessionStorage.setItem($(this).attr('id'), "true");
+
+            //turn off mouseoff event
+            $(this).off("mouseout");
+
+        }
+        else {
+            sessionStorage.setItem($(this).attr('id'), "false");
+            deselectLinkSplit(data, selectedClass);
+
+            //turn back on mouseoff event
+            $(".svg-link-split").mouseout(function() {
+                var data = $(this).data();
+                deselectLinkSplit(data, hoverClass);
+            });
+        }
+    });
+}
+
 function selectLink(data, c) {
     $("#" + data.linkid).attr("class", "svg-link svg-link-hover"); 
     $("#" + data.targetid).addClass(c); 
@@ -57,6 +103,18 @@ function selectLink(data, c) {
 
 function deselectLink(data, c) {
     $("#" + data.linkid).attr("class", "svg-link"); 
+    $("#" + data.targetid).removeClass(c); 
+    $("#" + data.sourceid).removeClass(c); 
+}
+
+function selectLinkSplit(data, c, linkClass) {
+    $("#" + data.linkid).attr("class", "svg-link-hidden-hover " + linkClass); 
+    $("#" + data.targetid).addClass(c); 
+    $("#" + data.sourceid).addClass(c); 
+}
+
+function deselectLinkSplit(data, c) {
+    $("#" + data.linkid).attr("class", "svg-link-hidden"); 
     $("#" + data.targetid).removeClass(c); 
     $("#" + data.sourceid).removeClass(c); 
 }
--- static/js/sql-viewer/link.js
+++ static/js/sql-viewer/link.js
@@ -32,36 +32,58 @@ SqlViewer.Link.prototype.drawColumnLink = function() {
     
     if (sx < tx) {
         sx = sx + sourceColumn.width() + 5;
-
+        sxCorrection = 5;
+        txCorrection = 10;
     }
     else {
         sx = sx - 10;
+        sxCorrection = -0;
     }
 
     if (tx < sx) {
         tx = tx + targetColumn.width() + 5;
+        sxCorrection = -3;
         txCorrection = 10;
     }
     else {
-        tx = tx - 10; 
+        tx = tx - 10;
+        txCorrection = -0;
     }
 
 
     if (this.link.element.draw == "full") {
-
-        SqlViewer.draw.line(sx,sy,tx + txCorrection ,ty).stroke({ width: 15, opacity: 0 }).attr({ 
+        sx2 = sx + sxCorrection;
+        tx2 = tx + txCorrection;
+        SqlViewer.draw.line(sx2,sy,tx2 ,ty).stroke({ width: 15, opacity: 0 }).attr({ 
             "class" : "svg-hoverlink",
             "data-linkid" : this.link.id,
             "data-sourceid" : this.data.source.columnId,
             "data-targetid" : this.data.target.columnId
         });
 
-        SqlViewer.draw.line(sx,sy,tx + txCorrection ,ty).stroke({ width: 1 }).attr({ 
+        SqlViewer.draw.line(sx2,sy,tx2,ty).stroke({ width: 1 }).attr({ 
             "stroke-dasharray" : "10 5", 
             "class" : "svg-link",
             "id"    : this.link.id
         });
+
+        SqlViewer.draw.rect(10,4).attr({ x: sx, y: sy, class : "svg-link"});
+        SqlViewer.draw.rect(10,4).attr({ x: tx, y: ty, class : "svg-link"});
     }
-    SqlViewer.draw.rect(10,4).attr({ x: sx, y: sy, class : "svg-link"});
-    SqlViewer.draw.rect(10,4).attr({ x: tx, y: ty, class : "svg-link"});
+    else {
+        //split
+        SqlViewer.draw.line(sx,sy,tx + txCorrection ,ty).stroke({ width: 5 }).attr({ 
+            "class" : "svg-link-hidden", 
+            "id"    : this.link.id
+        });
+
+        SqlViewer.draw.rect(10,4).attr({ x: sx, y: sy, class : "svg-link-split", 
+            "data-linkid" : this.link.id,
+            "data-sourceid" : this.data.source.columnId,
+            "data-targetid" : this.data.target.columnId});
+        SqlViewer.draw.rect(10,4).attr({ x: tx, y: ty, class : "svg-link-split",  
+            "data-linkid" : this.link.id,
+            "data-sourceid" : this.data.source.columnId,
+            "data-targetid" : this.data.target.columnId});
+    }
 }
