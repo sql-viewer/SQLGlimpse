eff5c40c088bb88d327fba55067d4abc747acfee
0xspx <spx@denkei.org>
2016-03-10 11:11:27 +0100

    heroku database fix

--- sqlviewer/settings/heroku.py
+++ sqlviewer/settings/heroku.py
@@ -3,5 +3,5 @@ from sqlviewer.settings.common import *
 
 DEBUG = True
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = db_from_env
 
