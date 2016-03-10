d147f860eb635519650db0ac00d743ecc52fe823
0xspx <spx@denkei.org>
2016-03-10 11:15:42 +0100

    heroku database fix

--- sqlviewer/settings/heroku.py
+++ sqlviewer/settings/heroku.py
@@ -3,5 +3,12 @@ from sqlviewer.settings.common import *
 
 DEBUG = True
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'] = db_from_env
+# we only need the engine name, as heroku takes care of the rest
+DATABASES = {
+		    "default": {
+		    	        "ENGINE": "django.db.backends.postgresql_psycopg2",
+		    	            }
+			}
+
+DATABASES['default'].update(db_from_env)
 
