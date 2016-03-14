128db7f5d852a07f06ba108ea16d37817e59d46e
Stefan Martinov <stefan.martinov@gmail.com>
2016-03-14 21:56:56 +0100

    Removed default settings from wsgi

--- sqlviewer/wsgi.py
+++ sqlviewer/wsgi.py
@@ -12,9 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sqlviewer.settings.develop")
-
-application = Cling(get_wsgi_application())
-
-
+application = get_wsgi_application()
+application = Cling(application)
