1f488f01902e6d02503717acc7f2c7c36dbcc7a7
smartinov <stefan.martinov@gmail.com>
2016-03-14 19:05:21 +0100

    Remove postgres from heroku app

--- sqlviewer/settings/heroku.py
+++ sqlviewer/settings/heroku.py
@@ -1,7 +1,6 @@
 from sqlviewer.settings.common import *
 
 DEBUG = True
-db_from_env = dj_database_url.config(conn_max_age=500)
 # we only need the engine name, as heroku takes care of the rest
 DATABASES = {
     'default': {
