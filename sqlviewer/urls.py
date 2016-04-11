e4afbab91b95eeb5b38c817215e504dddcfba00b
Stefan Martinov <stefan.martinov@gmail.com>
2016-04-11 22:00:40 +0200

    Updated URLs of imports

--- sqlviewer/urls.py
+++ sqlviewer/urls.py
@@ -22,23 +22,22 @@ from django.contrib import admin
 # Additionally, we include login URLs for the browsable API.
 from django.contrib.auth.views import login as login_view
 from django.contrib.auth.views import logout as logout_view
-from sqlviewer.glimpse.views import diagram_details_view, model_version_details_view, models_list_view, ModelView, VersionView, DiagramView, model_upload_view, \
-    version_search_view
+from sqlviewer.glimpse import views as glimpse_views
 
 api = [
-    url(r'^api/v1/models/(?P<model_id>\d+)?$', ModelView.as_view()),
-    url(r'^api/v1/models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)?$', VersionView.as_view()),
-    url(r'^api/v1/models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)/diagrams/(?P<diagram_id>\d+)?$', DiagramView.as_view()),
+    url(r'^api/v1/models/(?P<model_id>\d+)?$', glimpse_views.ModelView.as_view()),
+    url(r'^api/v1/models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)?$', glimpse_views.VersionView.as_view()),
+    url(r'^api/v1/models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)/diagrams/(?P<diagram_id>\d+)?$',glimpse_views. DiagramView.as_view()),
 ]
 pages = [
-    url(r'^$', models_list_view, name="home"),
+    url(r'^$', glimpse_views.models_list_view, name="home"),
     url(r'^admin/', admin.site.urls),
-    url(r'^models/upload$', model_upload_view, name='model_upload'),
+    url(r'^models/upload$', glimpse_views.model_upload_view, name='model_upload'),
     url(r'^accounts/login/$', login_view, name='login'),
     url(r'^accounts/logout/$', logout_view, name='logout'),
-    url(r'^models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)$', model_version_details_view, name='model_details'),
-    url(r'^models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)/search$', version_search_view, name='version_search'),
-    url(r'^models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)/diagrams/(?P<diagram_id>\d+)$', diagram_details_view, name='diagram_details'),
+    url(r'^models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)$', glimpse_views.model_version_details_view, name='model_details'),
+    url(r'^models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)/search$', glimpse_views.version_search_view, name='version_search'),
+    url(r'^models/(?P<model_id>\d+)/versions/(?P<version_id>\d+)/diagrams/(?P<diagram_id>\d+)$', glimpse_views.diagram_details_view, name='diagram_details'),
 ]
 
 urlpatterns = pages + api
